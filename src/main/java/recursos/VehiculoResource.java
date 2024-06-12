20feb5b2ed911d50507404a8a1bc17eb737ec099
Emerson <emerson.borda.1994@gmail.com>
2024-06-12 09:50:51 -0400

    Subiendo delete

--- src/main/java/recursos/VehiculoResource.java
+++ src/main/java/recursos/VehiculoResource.java
@@ -1,12 +1,17 @@
 package recursos;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import entity.VehiculoEntity;
+import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import repositorio.VehiculoRepository;
 
 
  //Para insertar datos en una BD se debe amrcar el endpoint como Transactional, en este caso está a nivel de clase, aplica para todos los endpoints
@@ -15,7 +20,8 @@ import jakarta.ws.rs.Path;
  public class VehiculoResource {
     //IMPLEMENTAMOS ENDPOINTSA
 
-
+    @Inject
+    private VehiculoRepository vehiculosRepository;
 
     @GET //Lista vehiculos
     public List<VehiculoEntity> index(){
@@ -28,4 +34,23 @@ import jakarta.ws.rs.Path;
         inserteVehiculo.persist();
         return inserteVehiculo;
     }
+
+    @GET
+    @Path("{id}")
+    public VehiculoEntity obtenerVehiculo(@PathParam("id") Long id){
+        var vehi = vehiculosRepository.findById(id);
+        if (vehi != null) {
+            return vehi;
+        }
+        throw new NoSuchElementException("No existen vehículos con el ID "+id);
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void eliminar(@PathParam("id") Long id){
+        vehiculosRepository.deleteById(id);
+        
+    }
+
+    
 }
